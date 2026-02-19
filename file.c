687f012f593abbc479665907c4142df7e83d69d8
calm1403 <harris.berry@outlook.com>
2026-02-19 21:15:26 +0000

    Made file.c readable (I lied about the final commit, mwahaha).

--- file.c
+++ file.c
@@ -9,23 +9,26 @@ create_fb(char* location)
   file_buffer_t* fb;
   if ((fb = malloc(sizeof *fb)) == NULL)
     return NULL;
-  fb->file_name = location;
-  fb->save = false;
+
+  *fb = (file_buffer_t){ .file_name = location, .save = false };
   if ((fb->file_pointer = fopen(fb->file_name, "r+")) == NULL)
   {
     free(fb);
     return NULL;
   }
+
   fseek(fb->file_pointer, 0L, SEEK_END);
   if ((fb->size = ftell(fb->file_pointer)) == 0)
     return NULL;
   fseek(fb->file_pointer, 0L, SEEK_SET);
+
   if ((fb->buffer = malloc(fb->size + 1)) == NULL)
   {
     fclose(fb->file_pointer);
     free(fb);
     return NULL;
   }
+
   fb->buffer[fb->size] = '\0';
   fread(fb->buffer, 1, fb->size, fb->file_pointer);
   return fb;
@@ -39,6 +42,7 @@ deallocate_fb(file_buffer_t* fb)
     fseek(fb->file_pointer, 0L, SEEK_SET);
     fwrite(fb->buffer, 1, fb->size, fb->file_pointer);
   }
+
   free(fb->buffer);
   fclose(fb->file_pointer);
   free(fb);
